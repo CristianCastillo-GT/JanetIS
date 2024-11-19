634768c8d2d939c9cd8881adfc3bb7a53345f3f1
CristianCastillo-GT <dcstillo@canella.com.gt>
2024-11-19 09:28:47 -0600

    Corrección mtd: getCarteraConsolidad
    Se cambia el uso de DI API para la ejecución de los SP de cartera consolidada y se reemplaza por consultas ADO

---
+++ CanellaMovilBackend/Controllers/SAPControllers/SAPDataService.cs
@@ -0,0 +1,78 @@
+﻿using CanellaMovilBackend.Models.SAPModels.Reports;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace CanellaMovilBackend.Controllers.SAPControllers
+{
+    public class SAPDataService
+    {
+        private string _connectionString;
+
+        public SAPDataService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SQLSAP");
+        }
+
+        public async Task<List<CarteraConsolidada>> GetCarteraConsolidadaAsync(string storedProcedure, int empresa)
+        {
+            var cartera = new List<CarteraConsolidada>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand(storedProcedure, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var item = new CarteraConsolidada
+                            {
+                                DocEntry = reader["DocEntry"].ToString(),
+                                DocNum = reader["DocNum"].ToString(),
+                                TransID = reader["TransID"].ToString(),
+                                CodVendedor = reader["CodVendedor"].ToString(),
+                                NomVendedor = reader["NomVendedor"].ToString(),
+                                FechaFacturacion = reader["FechaFacturacion"].ToString(),
+                                FechaVence = reader["FechaVence"].ToString(),
+                                PagoNumero = reader["PagoNumero"].ToString(),
+                                FacturaSerie = reader["FacturaSerie"].ToString(),
+                                FacturaNumero = reader["FacturaNumero"].ToString(),
+                                NumeroDocumento = reader["NumeroDocumento"].ToString(),
+                                CentroCosto = reader["CentroCosto"].ToString(),
+                                TipoDocumento = reader["TipoDocumento"].ToString(),
+                                TipoPago = reader["TipoPago"].ToString(),
+                                ClienteCodigo = reader["ClienteCodigo"].ToString(),
+                                ClienteNombre = reader["ClienteNombre"].ToString(),
+                                GrupoCliente = reader["GrupoCliente"].ToString(),
+                                DireccionFISCAL = reader["DireccionFISCAL"].ToString(),
+                                CobradorCodigo = reader["CobradorCodigo"].ToString(),
+                                CobradorNombre = reader["CobradorNombre"].ToString(),
+                                TotalFactura = reader["TotalFactura"].ToString(),
+                                TotalCuota = reader["TotalCuota"].ToString(),
+                                TotalPagado = reader["TotalPagado"].ToString(),
+                                TotalPagadoxCuota = reader["TotalPagadoxCuota"].ToString(),
+                                TotalSaldo = reader["TotalSaldo"].ToString(),
+                                SALDO_0_30 = reader["SALDO_0_30"].ToString(),
+                                SALDO_31_60 = reader["SALDO_31_60"].ToString(),
+                                SALDO_61_90 = reader["SALDO_61_90"].ToString(),
+                                SALDO_91_120 = reader["SALDO_91_120"].ToString(),
+                                SALDO_120_ = reader["SALDO_120_"].ToString(),
+                                AL_DIA = reader["AL_DIA"].ToString(),
+                                MORA = reader["MORA"].ToString(),
+                                Estado = reader["Estado"].ToString()
+                            };
+                            cartera.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return cartera;
+        }
+    }
+
+}
---
+++ CanellaMovilBackend/Static/Utils/ObjectConvert.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Reflection;
+
+namespace CanellaMovilBackend.Static.Utils
+{
+    /// <summary>
+    /// Convierte un datatable en una clase
+    /// </summary>
+    public class ObjectConvert
+    {
+        /// <summary>
+        /// Obtiene el datatable y lo convierte en un listado del objecto
+        /// </summary>
+        public static List<T> CreateListFromDataTable<T>(DataTable tbl) where T : new()
+        {
+            List<T> lst = [];
+
+            foreach (DataRow r in tbl.Rows)
+            {
+                lst.Add(CreateItemFromRow<T>(r));
+            }
+            return lst;
+        }
+        /// <summary>
+        /// Obtiene el datarow y lo inserta
+        /// </summary>
+        private static T CreateItemFromRow<T>(DataRow row) where T : new()
+        {
+            T item = new();
+            SetItemFromRow(item, row);
+            return item;
+        }
+
+        /// <summary>
+        /// Obtiene el item y lo ingresa al listado
+        /// </summary>
+        private static void SetItemFromRow<T>(T item, DataRow row) where T : new()
+        {
+            foreach (DataColumn c in row.Table.Columns)
+            {
+                PropertyInfo? p = item?.GetType().GetProperty(c.ColumnName);
+                if (p != null && row[c] != DBNull.Value)
+                {
+                    p.SetValue(item, row[c], null);
+                }
+            }
+        }
+    }
+}
